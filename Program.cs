a88c06c787b3a9b8e027c8eefa634510734259f2
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-01 16:57:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Введите путь к исходному текстовому файлу:");
+            string? sourceFilePath = Console.ReadLine();
+
+            if (!File.Exists(sourceFilePath))
+            {
+                Console.WriteLine("Ошибка: указанный файл не существует.");
+                return;
+            }
+
+            Console.WriteLine("Введите путь и имя нового файла:");
+            string? destinationFilePath = Console.ReadLine();
+
+            string fileContent = File.ReadAllText(sourceFilePath);
+
+            string upperCaseContent = fileContent.ToUpper();
+
+            File.WriteAllText(destinationFilePath, upperCaseContent);
+
+            Console.WriteLine($"Файл успешно скопирован в '{destinationFilePath}' с преобразованием символов в верхний регистр.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
